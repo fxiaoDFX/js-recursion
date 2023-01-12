dfb6c3d083dca4a3939f9b856ae9a2326d277249
Frankie Xiao <fxiaoX089@live.com>
2023-01-12 14:37:19 -0600

    remove mergeSort iife

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ import merge from "./mergeSort.js"
 
 console.log(fibonacci.fibs(5))
 console.log(fibonacci.fibsRec(5))
-console.log(merge.sort([2, 3, 1, 1, 23, 4]))
-console.log(merge.sort(["b", "a", 1, 0, 23, 1, 7, "A", "!", "Z"]))
+console.log(merge([2, 3, 1, 1, 23, 4]))
+console.log(merge(["b", "a", 1, 0, 23, 1, 7, "A", "!", "Z"]))
--- mergeSort.js
+++ mergeSort.js
@@ -1,23 +1,17 @@
-const merge = (() => {
-    function sort(array) {
-        if (array.length < 2) return array
+export default function mergeSort(array) {
+    if (array.length < 2) return array
 
-        const mid = Math.floor(array.length / 2)
+    const mid = Math.floor(array.length / 2)
 
-        const left = sort(array.slice(0, mid))
-        const right = sort(array.slice(mid, array.length))
+    const left = mergeSort(array.slice(0, mid))
+    const right = mergeSort(array.slice(mid, array.length))
 
-        const sorted = []
-        while (left.length || right.length) {
-            left[0] < right[0] || right[0] === undefined
-                ? sorted.push(left.shift())
-                : sorted.push(right.shift())
-        }
-
-        return sorted
+    const sorted = []
+    while (left.length || right.length) {
+        left[0] < right[0] || right[0] === undefined
+            ? sorted.push(left.shift())
+            : sorted.push(right.shift())
     }
 
-    return { sort }
-})()
-
-export default merge
+    return sorted
+}
